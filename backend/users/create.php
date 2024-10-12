61c054cef2ec514f9d8ca18859c7d853766477af
UEmmanuel5 <ugwuemmanuelugo2000@gmail.com>
2024-10-13 02:59:24 +0800

    login_commits

--- backend/users/create.php
+++ backend/users/create.php
@@ -19,3 +19,61 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 $conn->close();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    
+    <!-- font awesome begin -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <!-- font awesome end  -->
+
+    <link rel="stylesheet" href="src/components/login_page/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="login-box">
+        <h1>Login</h1>
+
+        <form action="">
+            <div class="input1">
+                <label for="username">
+                    <input type="text" id="username"  placeholder="Username" required>
+                </label>
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="input2">
+                <label for="password">
+                    <input type="password" id="password"  placeholder="Password" required>
+                </label>
+                <i class="fa-solid fa-lock"></i>
+            </div>
+
+            <div class="input3_forgotpwd">
+                <div class="input3">
+                    <input type="checkbox" id="remember_me">
+                    <label for="remember_me" class="rem">Remember me</label>
+                </div>
+                <a href="#" class="forgot_password">Forgot Password</a>
+                    
+
+            </div>
+
+            <input type="submit" class="submitbtn"><br>
+            <span>Don't have an account yet?</span><a href="#" class="signup">Sign up</a>
+
+        </form>
+
+
+    </div>
+    
+</body>
+</html>
